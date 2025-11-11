157abd3430dbc6c41a554b547a478f1863ac885b
Rutger van Haasteren <vhaasteren@gmail.com>
2025-11-11 10:41:49 +0100

    Updated psrchive to main repo again

--- scripts/build_psrchive.sh
+++ scripts/build_psrchive.sh
@@ -7,9 +7,7 @@ set -euo pipefail
 # Requires: HEALPix (optional), CALCEPH, TEMPO2, PGPLOT env, numpy present in venv
 
 cd "${SOFTWARE_DIR}"
-# Original: git clone --depth=1 git://git.code.sf.net/p/psrchive/code psrchive
-# Temporary: Using vhaasteren's GitHub fork with healpix branch that includes the epsic submodule fix
-git clone --depth=1 -b healpix https://github.com/vhaasteren/psrchive.git psrchive
+git clone --depth=1 git://git.code.sf.net/p/psrchive/code psrchive
 cd psrchive
 ./bootstrap
 
